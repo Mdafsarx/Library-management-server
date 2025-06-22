ffbee21fe78956d1be4de7a118212ff4dc4e3f48
Mdafsarx <mdafsar.dev@gmail.com>
2025-06-22 23:58:13 +0600

    replace name error to errors

--- src/middlewares/globalErrorHandler.ts
+++ src/middlewares/globalErrorHandler.ts
@@ -12,7 +12,7 @@ export const globalErrorHandler: ErrorRequestHandler = (
     res.status(statusCode).json({
       message: message,
       success: false,
-      error: err,
+      errors: err,
     });
   }
 };
